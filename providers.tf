531c41293bb2c2973f34e068000e6fd6cade1c0b
Par <pacroy@gmail.com>
2021-12-13 16:26:03 +0700

    Set s3 bucket and key

--- providers.tf
+++ providers.tf
@@ -5,7 +5,10 @@ terraform {
       version = "~> 3.0"
     }
   }
-  backend "s3" {}
+  backend "s3" {
+    bucket = "partfbackend"
+    key    = "test-terraform-aws/terraform.tfstate"
+  }
 }
 
 provider "aws" {}
