a4b6a87c6f38414c92022ac61c10f6d5f8595233
Par <pacroy@gmail.com>
2021-12-15 14:52:55 +0700

    Add AWS IAM TF files

---
+++ aws-iam/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_policy" "terraform_partfbackend" {
+  name        = "terraform_partfbackend"
+  path        = "/"
+  description = "Allow to use S3 and DynamoDB named partfbackend as Terraform backend"
+
+  policy = file("${path.module}/terraform_partfbackend.json")
+}
+
+resource "aws_iam_policy" "deny_ec2_permissions" {
+  name        = "deny_ec2_permissions"
+  path        = "/"
+  description = "Deny all EC2 permissions management actions"
+
+  policy = file("${path.module}/deny_ec2_permissions.json")
+}
+
+resource "aws_iam_group" "terraform_partfbackend" {
+  name = "terraform_partfbackend"
+  path = "/users/"
+}
+
+resource "aws_iam_group" "vpc_contributor" {
+  name = "vpc_contributor"
+  path = "/users/"
+}
+
+resource "aws_iam_group_policy_attachment" "terraform_partfbackend" {
+  group      = aws_iam_group.terraform_partfbackend.name
+  policy_arn = aws_iam_policy.terraform_partfbackend.arn
+}
+
+resource "aws_iam_group_policy_attachment" "vpc_contributor" {
+  group      = aws_iam_group.vpc_contributor.name
+  policy_arn = aws_iam_policy.deny_ec2_permissions.arn
+}
+
+resource "aws_iam_user" "test_terraform_aws" {
+  name = "test_terraform_aws"
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "test_terraform_aws" {
+  user = aws_iam_user.test_terraform_aws.name
+}
+
+resource "aws_iam_user_group_membership" "test_terraform_aws" {
+  user = aws_iam_user.test_terraform_aws.name
+
+  groups = [
+    aws_iam_group.terraform_partfbackend.name,
+    aws_iam_group.vpc_contributor.name,
+  ]
+}
---
+++ aws-iam/outputs.tf
@@ -0,0 +1,24 @@
+output "policy_terraform_partfbackend" {
+  description = "AWS IAM Policy terraform_partfbackend"
+  value       = aws_iam_policy.terraform_partfbackend
+}
+
+output "policy_deny_ec2_permissions" {
+  description = "AWS IAM Policy deny_ec2_permissions"
+  value       = aws_iam_policy.deny_ec2_permissions
+}
+
+output "group_aws_iam_group" {
+  description = "AWS IAM Group terraform_partfbackend"
+  value       = aws_iam_group.terraform_partfbackend
+}
+
+output "group_vpc_contributor" {
+  description = "AWS IAM Group vpc_contributor"
+  value       = aws_iam_group.vpc_contributor
+}
+
+output "user_test_terraform_aws" {
+  description = "AWS IAM User test_terraform_aws"
+  value       = aws_iam_user.test_terraform_aws
+}
