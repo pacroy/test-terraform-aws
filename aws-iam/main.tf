bf73d228e7eee4772533fbbe4d0717275ad7643b
Par <pacroy@gmail.com>
2021-12-16 10:20:45 +0700

    Replace terraform_partfbackend w/ terraform_users

--- aws-iam/main.tf
+++ aws-iam/main.tf
@@ -30,28 +30,38 @@ resource "aws_iam_policy" "deny_ec2_permissions" {
   policy = file("${path.module}/deny_ec2_permissions.json")
 }
 
-resource "aws_iam_group" "terraform_partfbackend" {
-  name = "terraform_partfbackend"
+resource "aws_iam_group" "terraform_users" {
+  name = "terraform_users"
   path = "/users/"
 }
 
-resource "aws_iam_group" "vpc_contributor" {
-  name = "vpc_contributor"
+resource "aws_iam_group" "vpc_contributors" {
+  name = "vpc_contributors"
   path = "/users/"
+}
+
+resource "aws_iam_group_policy_attachment" "allow_getuser_self" {
+  group      = aws_iam_group.terraform_users.name
+  policy_arn = aws_iam_policy.allow_getuser_self.arn
+}
+
+resource "aws_iam_group_policy_attachment" "allow_assume_any_roles" {
+  group      = aws_iam_group.terraform_users.name
+  policy_arn = aws_iam_policy.allow_assume_any_roles.arn
 }
 
 resource "aws_iam_group_policy_attachment" "terraform_partfbackend" {
-  group      = aws_iam_group.terraform_partfbackend.name
+  group      = aws_iam_group.terraform_users.name
   policy_arn = aws_iam_policy.terraform_partfbackend.arn
 }
 
-resource "aws_iam_group_policy_attachment" "vpc_contributor_deny_ec2_permissions" {
-  group      = aws_iam_group.vpc_contributor.name
+resource "aws_iam_group_policy_attachment" "deny_ec2_permissions" {
+  group      = aws_iam_group.vpc_contributors.name
   policy_arn = aws_iam_policy.deny_ec2_permissions.arn
 }
 
-resource "aws_iam_group_policy_attachment" "vpc_contributor_vpc_fullaccess" {
-  group      = aws_iam_group.vpc_contributor.name
+resource "aws_iam_group_policy_attachment" "amazonvpcfullaccess" {
+  group      = aws_iam_group.vpc_contributors.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
 
@@ -68,7 +78,7 @@ resource "aws_iam_user_group_membership" "test_terraform_aws" {
   user = aws_iam_user.test_terraform_aws.name
 
   groups = [
-    aws_iam_group.terraform_partfbackend.name,
-    aws_iam_group.vpc_contributor.name,
+    aws_iam_group.terraform_users.name,
+    aws_iam_group.vpc_contributors.name,
   ]
 }
