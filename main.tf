18315b9bb325d066c56d9f67255bc4f5693cea53
Par <pacroy@gmail.com>
2021-12-16 13:58:36 +0700

    Add s3_bucket resource with more provider alias

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 resource "aws_vpc" "test" {
+  provider = aws.ec2_contributors
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_s3_bucket" "test" {
+  provider = aws.s3_contributors
+  bucket = "partestbucket"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "test" {
+  provider = aws.s3_contributors
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
